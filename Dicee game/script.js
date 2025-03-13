c642cdb7f473300dce8d945f307f26e8f8ad7ffe
uvatarkind <yeabsira.mekonnen.w@gmail.com>
2025-03-13 13:44:53 +0300

    Updated script.js

--- Dicee game/script.js
+++ Dicee game/script.js
@@ -3,49 +3,11 @@ function rolldice(){
 var p1=Math.floor(Math.random()*6)+1
 var p2=Math.floor(Math.random()*6)+1
 
-function img_chg(p1){
-    if(p1==1){
-        document.querySelector('.img1').setAttribute('src','images/dice1.png')
-    }
-    else if(p1==2){
-        document.querySelector('.img1').setAttribute('src','images/dice2.png')
-    }
-    else if(p1==3){
-        document.querySelector('.img1').setAttribute('src','images/dice3.png')
-    }
-    else if(p1==4){
-        document.querySelector('.img1').setAttribute('src','images/dice4.png')
-    }
-    else if(p1==5){
-        document.querySelector('.img1').setAttribute('src','images/dice5.png')
-    }
-    else{
-        document.querySelector('.img1').setAttribute('src','images/dice6.png')
-    }
+var p1_img= `images/dice${p1}.png`;
+var p2_img=`images/dice${p2}.png`;
 
-}
-function img_chg2(p2){
-    if(p2==1){
-        document.querySelector('.img2').setAttribute('src','images/dice1.png')
-    }
-    else if(p2==2){
-        document.querySelector('.img2').setAttribute('src','images/dice2.png')
-    }
-    else if(p2==3){
-        document.querySelector('.img2').setAttribute('src','images/dice3.png')
-    }
-    else if(p2==4){
-        document.querySelector('.img2').setAttribute('src','images/dice4.png')
-    }
-    else if(p2==5){
-        document.querySelector('.img2').setAttribute('src','images/dice5.png')
-    }
-    else{
-        document.querySelector('.img2').setAttribute('src','images/dice6.png')
-    }
-
-}
-
+document.querySelector('.img1').setAttribute('src',p1_img)
+document.querySelector('.img1').setAttribute('src',p2_img)
 
 if (p1>p2){
     document.querySelector('h1').textContent ='Player1 Wins'
@@ -56,9 +18,6 @@ else if (p1<p2){
 else{
     document.querySelector('h1').textContent ='Draw'
 }
-
-img_chg(p1)
-img_chg2(p2)
 
 }
 window.onload=rolldice;
